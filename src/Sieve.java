5939ed739fcf52b1fb24aa1740235ae8d5608298
rbooth5665 <145709169+rbooth5665@users.noreply.github.com>
2025-09-27 14:46:21 -0400

    Add files via upload
    Includes the sieve class to handle data collection and management methods, improving readability for the driver class.

---
+++ src/Sieve.java
@@ -0,0 +1,15 @@
+//class that holds all data collection methods
+public class Sieve {
+    //flow rate
+    //Average wait
+    //Average speed
+    //number of vehicles, as well as specific type
+    //average light time
+    //frequency of light changing
+
+
+
+
+
+
+}
